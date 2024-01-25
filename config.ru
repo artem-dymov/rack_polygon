0a36298392a77083231e34cbb10138256505de57
Artem Dymov <dymov.artem@outlook.com>
2024-01-26 00:13:17 +0200

    Warden has been added

--- config.ru
+++ config.ru
@@ -2,10 +2,16 @@
 require "rack"
 require "rack/session/cookie"
 require_relative 'main'
+require "warden"
 
 use Rack::Session::Cookie, key: 'rack.session',
     path: '/',
     expire_after: 40, # Время жизни куки в секундах
-    secret: 'some_very_long_and_complex_secret_string_that_is_at_least_64_bytes_long1'
+    secret: 'some_very_long_and_complex_secret_string_that_is_at_least_64_bytes_long12'
+
+use Warden::Manager do |manager|
+  manager.default_strategies :basic
+  manager.failure_app = -> env { [401, {"content-type" => "text/html"}, ["<h1>Unauthorized</h1>"]] }
+end
 
 run App.new
--- main.rb
+++ main.rb
@@ -1,13 +1,29 @@
 require 'rack'
 require 'json'
+require 'warden'
+
+Warden::Strategies.add(:basic) do
+  def valid?
+    @params = JSON.parse env['rack.input'].read
+    @params["email"] || @params["password"]
+
+  end
+
+  def authenticate!
+    if @params["email"] == "admin@gmail.com" && @params["password"] == "0001"
+      success! "User object"
+    else
+      fail!
+    end
+  end
+end
 
 class App
   def call(env)
-    login_data = {email: "admin@gmail.com", password: "0001"}
     req = Rack::Request.new env
 
     res = Rack::Response.new
-    res.set_header 'Content-Type', 'text/html'
+    res.set_header 'content-type', 'text/html'
 
     case req.path_info
     when '/'
@@ -15,34 +31,26 @@ class App
     when '/public_article/'
       res.write File.read('views/public_article.html')
     when '/protected_article/'
+
       session = env['rack.session']
       session ||= {}
-      puts session
-      if session[:auth] == login_data
+      session[:auth] ||= {}
+      if session[:auth]["email"] == "admin@gmail.com" && session[:auth]["password"] == "0001"
         res.write File.read('views/protected_article.html')
       else
-        res.status = 403
-        res.body = ['Access denied.']
+        res.write "Access denied"
+        res.status = 401
       end
+
     when '/auth/'
       res.write File.read('views/auth.html')
     when '/auth_request/'
-      req_data_json = req.body.read
-      req_data = JSON.parse(req_data_json)
+      env['warden'].authenticate!
 
-      email = req_data['email']
-      password = req_data['password']
+      session = env['rack.session']
+      session ||= {}
+      session[:auth] = {"email" => "admin@gmail.com", "password" => "0001"}
 
-      if email.strip == login_data[:email] && password == login_data[:password]
-        res.write({email: email, password: password}.to_json)
-        puts login_data
-
-        session = env['rack.session']
-        session ||= {}
-        session[:auth] = login_data
-      else
-        res.status = 401
-      end
 
     else
       res.status = 404
@@ -53,4 +61,3 @@ class App
   end
 end
 
-# run App
